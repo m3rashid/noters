6969c1b6ff92dac5f05fc26ec3356f263ca5a69b
m3rashid <m3rashid.hussain@gmail.com>
2024-05-11 21:23:34 +0530

    finishing touch

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -3,8 +3,8 @@ import { useNavigate } from '@tanstack/react-router'
 import { useAuth } from '../hooks/auth'
 
 function Header() {
-	const navigate = useNavigate()
 	const { auth } = useAuth()
+	const navigate = useNavigate()
 
 	const handleClick = () => {
 		if (auth.isAuthenticated) {
@@ -18,7 +18,9 @@ function Header() {
 	return (
 		<div className="flex h-12 items-center justify-between gap-2 border-b border-gray-200 p-2">
 			<div className="flex select-none items-center gap-4">
-				<h2 className="text-lg font-bold">Noters</h2>
+				<h2 onClick={() => navigate({ to: '/' })} className="text-lg font-bold">
+					Noters
+				</h2>
 			</div>
 
 			<div className="flex items-center gap-4">
--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,13 +1,14 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import Header from '../components/header';
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+import Header from '../components/header'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
+	component: () => (
+		<>
 			<Header />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
-});
+			<Outlet />
+			{import.meta.env.PROD ? null : <TanStackRouterDevtools />}
+		</>
+	),
+})
